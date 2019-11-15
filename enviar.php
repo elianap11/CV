54342f3c239d63f32f873ed13fd1cd4199b3b99b
Eliana Paniagua <39173046+elianap11@users.noreply.github.com>
2019-11-15 13:57:33 -0300

    Delete enviar.php

--- enviar.php
+++
@@ -1,9 +0,0 @@
-<?php
-$destino = "elianaprensa@gmail.com";
-$nombre = $_POST["nombre"];
-$correo = $_POST["correo"];
-$mensaje = $_POST["mensaje"];
-$contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nMensaje: " . $mensaje;
-mail($destino, "Contacto", $contenido);
-header("Location:gracias.html");
-?>
